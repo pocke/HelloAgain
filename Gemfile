a2d98fc2d0994af54e6828cfedb56bc36849f3df
sinshutukibotu <c0114292a2@edu.teu.ac.jp>
2016-02-13 13:07:56 +0900

    gem 'omniauth-facebook' 追加

--- Gemfile
+++ Gemfile
@@ -32,6 +32,7 @@ gem 'bcrypt', '~> 3.1.7'
 gem 'mongoid'
 gem 'mongoid-geospatial'
 gem 'highline'
+gem 'omniauth-facebook'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -262,4 +262,7 @@ Devise.setup do |config|
   # When using OmniAuth, Devise cannot automatically set OmniAuth path,
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
+  Rails.application.config.middleware.use OmniAuth::Builder do
+        provider :facebook, 752897084840723, 739daf0175d8dad8e746528b1cf61159
+  end
 end
