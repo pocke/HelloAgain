399417d11d0c41d39874a2174ab226b155dfd7aa
Masataka Kuwabara <p.ck.t22@gmail.com>
2016-02-13 10:31:44 +0900

    Redirect to cards_path

--- app/controllers/roots_controller.rb
+++ app/controllers/roots_controller.rb
@@ -2,8 +2,7 @@ class RootsController < ApplicationController
   # GET /roots
   def index
     if current_user
-      # TODO
-      sign_out current_user
+      redirect_to cards_path
     else
       redirect_to new_user_session_path
     end
