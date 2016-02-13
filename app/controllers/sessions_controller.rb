690ceeb0affe5d6b39852f6fa2c4af814aeca0c2
Masataka Kuwabara <p.ck.t22@gmail.com>
2016-02-13 09:47:18 +0900

    WIP login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,38 @@
 class SessionsController < ApplicationController
+  def sign_in
+    p = params.require(:user).permit(:email, :password)
+    email, password = p[:email], p[:password]
+    user = User.find_by!(email: email)
+
+    unless user.authenticate(password)
+      raise UnauthorizedError, "認証に失敗しました。メールアドレスとパスワードを確認して下さい。"
+    end
+
+    session[:user_id] = user.id
+    token = SecureRandom.hex(32)
+    session[:csrf_token] = token
+
+    meta = {
+      status: true,
+      csrf_token: token,
+      message: 'ログインが完了しました',  # TODO: I18n
+    }
+
+    opt = render_option(user, status: 200)
+    opt[:meta] = meta
+
+    render opt
+  end
+
+  def sign_up
+    p = params.require(:user).permit(:email, :password)
+    email, password = p[:email], p[:password]
+
+    if User.exists?(email: email)
+      raise "User #{email} is exist"
+    end
+
+    u = User.new(email: email, password: password)
+    u.save!
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User
   include Mongoid::Document
   include Mongoid::Attributes::Dynamic
+
+  has_secure_password
 end
