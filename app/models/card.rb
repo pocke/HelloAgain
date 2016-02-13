8733b436ba5939141274f1567e574e7ba6c11b6e
Masataka Kuwabara <p.ck.t22@gmail.com>
2016-02-13 11:00:56 +0900

    User has one card

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,4 +1,7 @@
 class Card
   include Mongoid::Document
   include Mongoid::Attributes::Dynamic
+
+  belongs_to :user
+  field :user_id, type: Integer
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -23,6 +23,8 @@ class User
   field :current_sign_in_ip, type: String
   field :last_sign_in_ip,    type: String
 
+  has_one :card
+
   ## Confirmable
   # field :confirmation_token,   type: String
   # field :confirmed_at,         type: Time
