4913857034bc5a65e6891285acdbc303da246534
Masataka Kuwabara <p.ck.t22@gmail.com>
2016-02-13 13:06:13 +0900

    名刺作成画面

--- app/models/card.rb
+++ app/models/card.rb
@@ -3,5 +3,8 @@ class Card
   include Mongoid::Attributes::Dynamic
 
   field :name, type: String, default: ""
+  field :affiliation, type: String, default: ""
+  field :phone, type: String, default: ""
+  field :position, type: String, default: ""
   belongs_to :user
 end
